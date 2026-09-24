04d167239dfe53d209cc00998d4048985d70d335
Jinu Yi <96781019+dionisos198@users.noreply.github.com>
2026-09-24 21:42:04 +0900

    [level 2] Title: 서버 증설 횟수, Time: 0.02 ms, Memory: 81.2 MB -BaekjoonHub

--- 프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.java
+++ 프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.java
@@ -2,28 +2,32 @@ import java.util.*;
 class Solution {
     public int solution(int[] players, int m, int k) {
         
-        int currentServer = 0;
-        Map<Integer, Integer> deleteServerMap = new HashMap<>();
-        
+        int currentServerCount = 0;
+        int reservedDelete[] = new int[25];
+        Arrays.fill(reservedDelete,0);
         int answer = 0;
-        for(int time = 0; time< players.length; time++){
-            int currentPlayerNum = players[time];
-       //     System.out.println("게임 이용자의 수: "+currentPlayerNum);
-            // 서버 제거 
-            if(deleteServerMap.containsKey(time)){
-                currentServer -= deleteServerMap.get(time);
+        
+        for(int i=0;i<players.length;i++){
+            
+            
+            currentServerCount -= reservedDelete[i];
+            int needServerCount = players[i] / m;
+          //  System.out.println("필요 서버 수: "+ needServerCount);
+            
+            if(needServerCount > currentServerCount){
+                
+                int needCount = needServerCount - currentServerCount;
+                if(i+k<=24){
+                    reservedDelete[i+k] += needCount;
+                }
+                answer += needCount;
+                currentServerCount = needServerCount;
             }
-            //서버 보충 
-            int minumumNeedServer= currentPlayerNum / m;
-            if(currentServer < minumumNeedServer){
-                answer += minumumNeedServer - currentServer;
-                deleteServerMap.put(time+k, minumumNeedServer - currentServer);
-                currentServer = minumumNeedServer;
-            }
-      //      System.out.println("현재 서버 수: "+currentServer);
+        
+         //   System.out.println("현재 서버 수: "+currentServerCount);
+            
         }
         
         return answer;
-        
     }
 }
